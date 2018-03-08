7c798e1bc0f40549cd1a973ebbe11b70731f9b81
eshy96 <emathur360@gmail.com>
2018-03-07 17:50:27 -0800

    adding yelp pod

---
+++ CapstoneProject/CapstoneProject/FeedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FeedViewController.swift
+//  CapstoneProject
+//
+//  Created by Eshita Mathur on 3/1/18.
+//  Copyright © 2018 Eshita Mathur. All rights reserved.
+//
+
+import UIKit
+import YelpAPI
+
+
+
+class FeedViewController: UIViewController {
+
+    @IBAction func ActiveYelpAPI(_ sender: Any) {
+       
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CapstoneProject/CapstoneProject/Yelp API/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CapstoneProject
+//
+//  Created by Eshita Mathur on 3/1/18.
+//  Copyright © 2018 Eshita Mathur. All rights reserved.
+//
+
+
+
+
